4e8be8552ca978c26eee493d9acdbd17c2c20d25
frankydanielsinaga <frankydanielsinaga@gmail.com>
2019-11-29 11:30:12 +0700

    p

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../Context';
 
 class Product extends Component {
     render() {
+        const {id,title,img,price,inCart} = this.props.product;
         return (
-            <div>
-                <h3>Ini Halaman Produk</h3>
-            </div>
+            <ProdukWarpper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={console.log('clicked')
+                    }>
+                        <Link to='/details'>
+                        <img src={img} alt="product" classname="card-img-top"/>
+                        </Link>
+
+                    </div>
+                    
+                </div>
+            </ProdukWarpper>
         );
     }
 }
 
 export default Product;
+
+const ProdukWarpper = styled.div``;
